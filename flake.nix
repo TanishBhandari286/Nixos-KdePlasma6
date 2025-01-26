4594137e537b72e4fb0e579ef96597f12e1fecea
TanishBhandari286 <tanishbhandari91@gmail.com>
2025-01-26 12:28:10 +0545

    added the initial config

---
+++ flake.nix
@@ -0,0 +1,17 @@
+
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      tanish = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
+
